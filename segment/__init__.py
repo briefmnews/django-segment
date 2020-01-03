93ae34e42297816b06deea596323d3eca04803b8
Thomas C <thomas.carbillet@gmail.com>
2020-01-03 11:26:51 +0100

    feat(): release version 0.1.1

--- segment/__init__.py
+++ segment/__init__.py
@@ -7,7 +7,7 @@ Easy integration of Segment analytics into Django projects.
 """
 
 __title__ = 'segment'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Michael Pedersen'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 SteelSeries'
